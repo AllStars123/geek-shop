0d39b59b02c3e8f363b76c8751bbdffafda23d10
AllStars123 <purnov.nikita@yandex.ru>
2021-11-18 01:22:45 +0300

    add HW_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'title': 'Geekshop Store'
+    }
+    return render(request, 'mainapp/index.html', context=context)
 
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'title': 'geekshop'
+    }
+    return render(request, 'mainapp/products.html', context=context)
